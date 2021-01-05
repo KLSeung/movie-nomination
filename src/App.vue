86615a9b29b62a3bd1fa3fc03cbbddf1e400ff0e
Kevin Lee <kevin@inorbis.ca>
2021-01-05 02:01:54 -0800

    initialize veutify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <div class="d-flex align-center">
+        <h2>The Shoppies</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href=""
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Login</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <SearchMovieBar/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchMovieBar from './components/SearchMovieBar';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SearchMovieBar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>

afd7f23d77766460334ae3bcf964a96afe657527
Kevin Lee <kevin@inorbis.ca>
2021-01-16 02:26:10 -0800

    change api request to be with https instead of http

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const actions = {
   fetchMovies({ commit, state }) {
     return new Promise((resolve, reject) => {
-      axios.get(`http://www.omdbapi.com/?apikey=256754b1&s=${state.movieSearchText}&page=${state.movieListPage}&type=movie`)
+      axios.get(`https://www.omdbapi.com/?apikey=256754b1&s=${state.movieSearchText}&page=${state.movieListPage}&type=movie`)
         .then(res => {
           commit('EMPTY_MOVIE_LIST')
           if (res.data.Response === 'True') {
@@ -13,7 +13,7 @@ const actions = {
           } else if (res.data.Error === 'Too many results.') {
             // This is for the case when there are too many results, but still should have a result
             // An example of a movie like this would be "Up" which
-            axios.get(`http://www.omdbapi.com/?apikey=256754b1&t=${state.movieSearchText}&page=${state.movieListPage}&type=movie`)
+            axios.get(`https://www.omdbapi.com/?apikey=256754b1&t=${state.movieSearchText}&page=${state.movieListPage}&type=movie`)
               .then(singleSearchRes => {
                 commit('UPDATE_MOVIES', res.data)
                 commit('UPDATE_PAGE_LENGTH', 1)
